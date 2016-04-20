fe69f6cfd57638d1761fec2d8624724d38d11c32
Anmol Mohanty <anmolnehru@gmail.com>
2016-04-19 19:00:47 -0500

    Beginning coding project 3

---
+++ Assignment 3/neuralnet.py
@@ -0,0 +1,9 @@
+#Author - Anmol Mohanty
+#Date :: 4/19
+
+if __name__ == "__main__":
+
+    #training file pulled from cmd
+    tf = sys.argv[1]
+    #open the file in read mode
+    tfp=open(tf,"r")
